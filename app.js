ebb52a15e7e1fe41a34ace84ce9d36e7aafbf704
Arbias <52889109+k-arbias@users.noreply.github.com>
2019-11-15 17:38:17 +0100

    Initializes game values and UI Elements

---
+++ app.js
@@ -0,0 +1,12 @@
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
